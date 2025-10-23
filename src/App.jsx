f99cc574493387e0dbe9f13cd91f906268d6203c
Akhrorbek1998 <developeraxrorbek@gmail.com>
2025-10-23 14:36:43 +0500

    update nav elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes, useNavigate } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Contacts from './pages/Contacts.jsx'
 
 const App = () => {
+  const navigate = useNavigate()
   return (
     <>
       <header className='bg-blue-500 text-white p-6'>
         <nav className={`flex justify-center gap-4 items-center`}>
-          <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/contacts'>Contacts</Link>
+          <button onClick={() => navigate('/')}>Home</button>
+          <button onClick={() => navigate('/about')}>About</button>
+          <button onClick={() => navigate('/contacts')}>Contacts</button>
           <button className='px-5 py-2 bg-red-500 text-white rounded-md'>Login</button>
         </nav>
       </header>
